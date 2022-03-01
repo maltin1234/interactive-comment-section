60f278055c53ac3223efc6d7cea8ce841c348137
reddington12 <37213559+Reddington12@users.noreply.github.com>
2022-03-01 22:49:24 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -3,33 +3,35 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <div class="row">
-        <div class="column">
-          <div class="card">
-            <h3>Card 1</h3>
-            <p>Some text</p>
-            <p>Some text</p>
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <div className="left-button">left button</div>
+            <div className="profile-pic">profile</div>
+            <div className="author-name">author</div>
+            <div className="reply">reply</div>
+            <div className="main">main-content</div>
           </div>
         </div>
 
-        <div class="column">
-          <div class="card">
+        <div className="column">
+          <div className="card">
             <h3>Card 2</h3>
             <p>Some text</p>
             <p>Some text</p>
           </div>
         </div>
 
-        <div class="column">
-          <div class="card">
+        <div className="column">
+          <div className="card">
             <h3>Card 3</h3>
             <p>Some text</p>
             <p>Some text</p>
           </div>
         </div>
 
-        <div class="column">
-          <div class="card">
+        <div className="column">
+          <div className="card">
             <h3>Card 4</h3>
             <p>Some text</p>
             <p>Some text</p>
