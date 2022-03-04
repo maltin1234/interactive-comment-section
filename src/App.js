27de10e3641b4819cc93ffb55252eafdf35bee3c
reddington12 <37213559+Reddington12@users.noreply.github.com>
2022-03-04 22:18:03 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from "react";
 import "../src/App.css";
+import avatar from "./images/avatars/image-amyrobson.png";
+import reply from "./images/icon-reply.svg";
 
 function App() {
   return (
     <div className="container">
       <div className="card">
         <div className="card__column card__column--first">
-          <button className="button__left--icon">Btn</button>
+          <button className="button__left--icon">+</button>{" "}
+          <button className="button__left--icon">12</button>
+          <button className="button__left--icon">-</button>
         </div>
         <div className="card__column  card__column--second">
           <div className="card__row card__row--first">
-            <p className="text">Avatar</p>
-            <p className="text">username</p>
-            <p className="text">date</p>
-            <p className="text">re</p>
+            <img className="content img-avatar" alt="" src={avatar}></img>
+            <p className="content text-username">Amy Robson</p>
+            <p className="content text">1 may 2022</p>
+            <img className="content img-reply" alt="" src={reply}></img>
           </div>
+
           <div className="card__row card__row--second">
-            <p>text goes here</p>
+            <p className="text main-content">
+              I couldn't agree more with this. Everything moves so fast and it
+              always seems like everyone knows the newest library/framework. But
+              the fundamentals are what stay constant.
+            </p>
           </div>
         </div>
       </div>
